260f5635b7beb6b6984f6d1775ecec5f303bcef8
KeshavVarma <keshavvarma.vegiraju@gmail.com>
2018-06-08 15:50:15 +0530

    fixed reload bug in post

--- js/new.js
+++ js/new.js
@@ -1,41 +1,33 @@
-$(document).ready(function () {
-    var date = new Date();
-    $("#date").val(date);
-    $("form").submit(function (event) {
-       
-      var date = $("#date").val();
-        var ConsignmentNumber = $("#Consignment").val();
-        var order = $("#order").val();
-        var email = $("#email").val();
-        var phone = $("#phone").val();
-        var toAddr = $("#toAddr").val();
-        var fromAddr = $("#fromAddr").val();
-        var weight = $("#weight").val();
-        var dimensions = $("#dimensions").val();
-        var status;
-
-        var postdata = {            
-            'date and time' : date ,
-            'consignmentNumber' : ConsignmentNumber ,
-            'order' : order ,
-            'email' : email ,
-            'phone' : phone ,
-            'to address' : toAddr ,
-            'from address' : fromAddr ,
-            'weight' : weight ,
-            'dimensions' : dimensions ,
-            'status' : 'received',
-        };  
-
-
-
-        var newPostRef = firebase.database().ref();
-
-        newPostRef.push( postdata);
-
-    })
+var messagesRef = firebase.database().ref('data');
+var date = new Date();
+$("#date").val(date);
+$("form").submit(function (e) {
+    e.preventDefault();
+    var date = $("#date").val();
+    var ConsignmentNumber = $("#Consignment").val();
+    var order = $("#order").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var toAddr = $("#toAddr").val();
+    var fromAddr = $("#fromAddr").val();
+    var weight = $("#weight").val();
+    var dimensions = $("#dimensions").val();
+    var status;
+    var postdata = {
+        'date and time': date,
+        'consignmentNumber': ConsignmentNumber,
+        'order': order,
+        'email': email,
+        'phone': phone,
+        'to address': toAddr,
+        'from address': fromAddr,
+        'weight': weight,
+        'dimensions': dimensions,
+        'status': 'received',
+    };
+    var newPostRef = firebase.database().ref();
+    newPostRef.push(postdata);
+    $("form")[0].reset();
+    location.reload();
 })
-
-
-
-
+       
