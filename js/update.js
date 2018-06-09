0b4f19fa3e2ddcc76262a2cc421716e990533dc6
KeshavVarma <keshavvarma.vegiraju@gmail.com>
2018-06-09 19:29:12 +0530

    done with updater

--- js/update.js
+++ js/update.js
@@ -1,11 +1,42 @@
-$(document).ready(function () {
-    
+var dbRef = firebase.database().ref("/");
+
+var id;
+$("#get").submit(function (e) {
+    e.preventDefault();
+    var number = $("#number").val();
+    var data = [];
+    $(".data").text("null");
 
 
+    dbRef.on( "child_added", function(dataSnapshot){
+        id = dataSnapshot.key;
+        data = dataSnapshot.val();
+        
+        if (data.order == number){
+            console.log(data);
+            data = JSON.stringify(data);
+            $(".data").text(data);
+        }        
+
+    } );
+
+   
+        
 })
 
+$("#post").submit(function (send) {
+    send.preventDefault();
+    var updater = $("#updater").val();   
+   
+    dbRef.on( "child_added", function(dataSnapshot){
+        dbRef.child(id).update({"status" : updater});
+    } ); 
+        
+})
 
-
-
-
-
+dbRef.on("child_changed", snap =>  {
+    var change = snap.key;
+    data = snap.val();
+    data = JSON.stringify(data);
+    $(".data").text(data);
+});
