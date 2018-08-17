237480047789a950f6f6ea572d15b6486963c280
KeshavVarma <keshavvarma.vegiraju@gmail.com>
2018-08-17 22:27:43 +0530

    fixed untrecked files

--- js/maps.js
+++ js/maps.js
@@ -3,13 +3,13 @@ $("#post").submit(function (event) {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       var number = $("#vehicle").val();
-      var logitude = $("#lat").val();
+      var lagitude = $("#lat").val();
       var longitude = $("#long").val();
       var status = "on road" ;
       var current;
       var postdata = {
-        'logitude': logitude,
-        'longitude': longitude
+        'lat': lagitude,
+        'lng': longitude
       };
       
       var newPostRef = firebase.database().ref().child("track").child(number);
@@ -41,7 +41,7 @@ $(document).ready(function () {
 
             var trackRef = searchRef.child(id).child("location");
             var c =0;
-            trackRef.limitToLast(1).on('child_added', function(data) {
+            trackRef.limitToLast(1).on("", function(data) {
               c++;
              if (c==2){
               var temp = data.val();
@@ -129,35 +129,5 @@ function initMap() {
 
 
 
-
-// $(document).ready(function () {
-//   firebase.auth().onAuthStateChanged(function (user) {
-//       if (user) {
-//           var searchRef = firebase.database().ref().child('truck');
-//           var referance = [];
-//           searchRef.on("child_added", snap => {
-//               referance = snap.val();
-//               var id = snap.key;
-//               $("table").append("<tr class='target_row' id = " + id + ">" + "<td>" + referance.date_and_time + "</td>" + "<td class='target'>" + referance.consignment_Number + "</td>"
-//                   + "<td>" + referance.order + "</td>" + "<td class='target'>" + referance.email + "</td>" + "<td class='target'>" + referance.phone + "</td>"
-//                   + "<td>" + referance.to_address + "</td>" + "<td>" + referance.from_address + "</td>" +
-//                   "<td>" + referance.weight + "</td>" + "<td>" + referance.dimensions + "</td>" + "<td>" + referance.status  + "</tr>");
-//           });
-//           searchRef.on("child_changed", snap => {
-//               var change = snap.key;
-//               referance = snap.val();
-//               document.getElementById(change).innerHTML = "<td>" + referance.date_and_time + "</td>" + "<td>" + referance.consignment_Number + "</td>"
-//                   + "<td>" + referance.order + "</td>" + "<td>" + referance.email + "</td>" + "<td>" + referance.phone + "</td>"
-//                   + "<td>" + referance.to_address + "</td>" + "<td>" + referance.from_address + "</td>" +
-//                   "<td>" + referance.weight + "</td>" + "<td>" + referance.dimensions + "</td>" + "<td>" + referance.status  + "<td>"  + "</td>"
-//           });
-
-//       } else {
-//           $(".table").css("display","none");
-//           window.location.replace("index.html");
-//       }
-//   });
-
-// })
 
 
