cf92da6d5e8bee5c1bedb538f2f71a4c227310e6
KeshavVarma <keshavvarma.vegiraju@gmail.com>
2018-08-18 13:44:15 +0530

    track done

--- js/maps.js
+++ js/maps.js
@@ -27,41 +27,39 @@ $("#post").submit(function (event) {
 
 });
 
-var data = {};
-var jsonVariable = {};
-for (var i = 1; i < 3; i++) {
-  jsonVariable[i + 'name'] = 'name' + i;
-}
-console.log(jsonVariable);
+
+// -----------------------------------------
+
+
+
+var track = [] ;
+var keshav = { lat: 17.781277, lng: 83.376805 };
 
 $(document).ready(function () {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      var searchRef = firebase.database().ref().child('track');
-
-      searchRef.on("child_changed", snap => {
-        var current = snap.val().current;
-        var id = snap.key;
-        console.log(current);
-        console.log(id);
-
-
-        var trackRef = searchRef.child(id).child("location");
-
-        trackRef.on("child_added", snap => {
-          track = snap.val();
-          data[id] =
-            {
-              "current": current,
-              "logs": track
-            }
-
-        });
-
-        console.log(data);
-
-
+      var searchRef = firebase.database().ref().child('track');    
+      searchRef.on("child_changed", snap => {        
+        now = snap.val().current;
+        var lat = JSON.parse(now.lat);
+        var lng = JSON.parse(now.lng);
+        var uluru = { "lat":lat, "lng": lng }; 
+         
+        initMap(uluru,track);
+        
       });
+
+      var trackRef = searchRef.child("AP31CM2090").child("location");
+
+      trackRef.on("child_added", snap => {
+        var location = snap.val();
+        var nlat = JSON.parse(location.lat);
+        var nlng = JSON.parse(location.lng);
+        var ntrack = { "lat":nlat, "lng": nlng };  
+        track = track.concat(ntrack);
+        initMap(uluru,track);        
+      });
+
 
     } else {
       window.location.replace("index.html");
@@ -74,50 +72,51 @@ $(document).ready(function () {
 
 
 
-
-function initMap() {
-
-  var map = new google.maps.Map(document.getElementById('map'), {
+function initMap(t,c) {
+uluru = t;
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: { lat: 17.781194, lng: 83.377222 }
+    center: uluru
   });
-  // var contentString = '<div id="content">' +
+  var contentString = '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="firstHeading" class="firstHeading">event1</h1>' +
+    '<div id="bodyContent">' +
+    '<p><b>event1</b>lorem</p>' +
+    '<p>View on google maps: <a href="https://goo.gl/maps/H7fLhbDM13p">' +
+    'click here</a> ' +
+    '</p>' +
+    '</div>' +
+    '</div>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 500
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function () {
+    infowindow.open(map, marker);
+  });
 
-  //   '<h1 id="firstHeading" class="firstHeading">event1</h1>' +
-  //   '<div id="bodyContent">' +
-  //   '<p><b>event1</b>lorem</p>' +
-  //   '</div>' +
-  //   '</div>';
-  // var infowindow = new google.maps.InfoWindow({
-  //   content: contentString,
-  //   maxWidth: 500
-  // });
-  // var marker = new google.maps.Marker({
-  //   position: uluru,
-  //   map: map,
-  //   title: 'Uluru (Ayers Rock)'
-  // });
-  // marker.addListener('click', function () {
-  //   infowindow.open(map, marker);
-  // });
+  var Coordinates = c;
+  var Path = new google.maps.Polyline({
+    path: Coordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
 
-
-
-
-  // var coordinates = [
-  //   { lat: 17.781194, lng: 83.377222 },
-  //   { lat: 17.7811, lng: 83.3772 }
-  // ];
-  // var path = new google.maps.Polyline({
-  //   path: coordinates,
-  //   geodesic: true,
-  //   strokeColor: '#000000',
-  //   strokeOpacity: 1.0,
-  //   strokeWeight: 2
-  // });
-
-  // path.setMap(map);
+  Path.setMap(map);
+  
 }
+
+
+
 
 
 
